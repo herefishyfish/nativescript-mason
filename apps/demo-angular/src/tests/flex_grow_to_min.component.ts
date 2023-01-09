7fff08659e98cfd091aecc60113fc1c6002389c7
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2023-01-09 16:37:51 +0800

    initial setup

---
+++ apps/demo-angular/src/tests/flex_grow_to_min.component.ts
@@ -0,0 +1,14 @@
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+
+@Component({
+  standalone: true,
+  schemas: [NO_ERRORS_SCHEMA],
+  template: `
+    <ActionBar title="flex_grow_to_min"></ActionBar>
+    <TSCView id="test-root" style="min-height: 100dip; max-height: 500dip; width: 100dip; flex-direction: column;" backgroundColor="red">
+      <TSCView style="flex-grow: 1; flex-shrink: 1;" backgroundColor="green"></TSCView>
+      <TSCView style="height: 50dip;" backgroundColor="blue"></TSCView>
+    </TSCView>
+  `,
+})
+export class FlexGrowToMinComponent {}
